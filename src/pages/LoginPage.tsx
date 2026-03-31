790cdf380b9f41fa2e9bf0bbf4a12b218957f60c
nandaniluitel <nandanicode@gmail.com>
2026-03-31 16:39:33 -0700

    edit

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -95,7 +95,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8">
       <svg
-        className="mb-16 ml-24"
+        className="mb-16 ml-30"
         width="400"
         viewBox="0 0 380 170"
         xmlns="http://www.w3.org/2000/svg"
