95256e069b5fcbafaccd0e36cfe43044a297fe82
nandaniluitel <nandanicode@gmail.com>
2026-03-31 16:15:58 -0700

    editing logo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -94,7 +94,7 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center p-6">
-      <svg width="200" viewBox="0 0 680 220" xmlns="http://www.w3.org/2000/svg">
+      <svg width="200" viewBox="0 0 380 170" xmlns="http://www.w3.org/2000/svg">
         <style>{`
           .logo-dim { font-family: Arial, sans-serif; font-weight: 500; font-size: 52px; fill: #888; letter-spacing: -1px; }
           .logo-highlight { font-family: Arial, sans-serif; font-weight: 500; font-size: 52px; fill: #7c3aed; letter-spacing: -1px; }
